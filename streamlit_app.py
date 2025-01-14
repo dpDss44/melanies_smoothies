5dc3896aac1d6ca58476d4c451ea08f7eeafc34e
dpDss44 <daniel.petrucci.-nd@disney.com>
2025-01-14 16:21:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,6 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+#sf_df = st.datafrome(data=smoothiefroot_response.json(), use_container_width=True)

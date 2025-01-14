b526a36d622d4f7cc51dd070574edf838f4f897e
dpDss44 <daniel.petrucci.-nd@disney.com>
2025-01-14 16:13:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("My Parents New Health Diner")
+st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Breakfast Menu
+    """Choose the fruits you want in your custom Smoothie!
     """
 )
 
